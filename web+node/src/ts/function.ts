025a61497e3fd279ec1aebf4e897a97ad0267c6c
yayaluoya <yayaluoya@sina.com>
2022-09-14 18:09:31 +0800

    关于ts的Function的深层理解

--- web+node/src/ts/function.ts
+++ web+node/src/ts/function.ts
@@ -23,3 +23,33 @@ let a: Iterable<number> = {
 let _a = a[Symbol.iterator]();
 
 export default {};
+
+/** 简写版的调用签名 */
+let f: () => void;
+/** 完整版的调用签名，因为函数是一个可调用对象，所有用对象的形式来描述调用签名才能实现最多功能 */
+let f2: {
+    (): void;
+    (): number;
+};
+
+type f2_ = typeof f2;
+
+type f3 = {
+    (a: number): number;
+    (b: string): string;
+};
+
+let f3_: f3 = ((a) => {
+    return a;
+});
+let f3__ = f3_(1);
+
+function fun(a: () => void, a2: {
+    (): void
+}) { }
+
+function fun1(a: number): number;
+function fun1(a: string): string;
+function fun1(a: number | string): number | string {
+    return a;
+}
