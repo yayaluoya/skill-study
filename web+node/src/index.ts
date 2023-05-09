f556bd0d38ea141eceda2b6239a85c5c6aefdd23
yayaluoya2 <yayaluoya@sina.com>
2023-05-09 09:19:21 +0800

    -

--- web+node/src/index.ts
+++ web+node/src/index.ts
@@ -3,3 +3,13 @@ import { AAAA } from "./_d/index";
 console.log(wx);
 
 AAAA.A
+
+/**
+ * 关于类静态部分和实例部分类型的验证
+ */
+class A {
+    constructor(a: string) { }
+}
+
+type AC = typeof A;
+type a = ConstructorParameters<AC>
