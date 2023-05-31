838254a0992543239071bc73cfe189903e975b48
yayaluoya2 <yayaluoya@sina.com>
2023-05-31 10:37:11 +0800

    关于https的东西

--- web+node/node/http-http.js
+++ web+node/node/http-http.js
@@ -6,13 +6,14 @@ const http = require('http');
 
 http.createServer((req, res) => {
     res.writeHead(200, {});
-    setTimeout(() => {
-        res.end(
-            JSON.stringify({
-                url: req.url,
-                header: req.headers,
-                time: Date.now(),
-            }),
-        );
-    }, 100);
-}).listen(2002);
+    res.end('hello world\n');
+    // setTimeout(() => {
+    //     res.end(
+    //         JSON.stringify({
+    //             url: req.url,
+    //             header: req.headers,
+    //             time: Date.now(),
+    //         }),
+    //     );
+    // }, 100);
+}).listen(2001);
---
+++ web+node/node/https.js
@@ -0,0 +1,15 @@
+const https = require('node:https');
+const fs = require('node:fs');
+const path = require('path');
+
+const options = {
+    key: fs.readFileSync(path.join(__dirname, './pem/privkey.pem')),
+    cert: fs.readFileSync(path.join(__dirname, './pem/cacert.pem')),
+};
+
+https
+    .createServer(options, (req, res) => {
+        res.writeHead(200);
+        res.end('hello world\n');
+    })
+    .listen(2003);
