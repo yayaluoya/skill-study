5737581a72c6da3b8543e5a00ff1ca98e48dd9eb
yayaluoya2 <yayaluoya@sina.com>
2023-08-01 13:47:13 +0800

    关于进程间IPC通信

--- web+node/node/child_process/b.js
+++ web+node/node/child_process/b.js
@@ -1,17 +1,34 @@
 const { spawn, exec, spawnSync, execFile } = require('child_process');
 const path = require('path');
+const net = require('net');
 
 console.log('node衍生shell的执行路径', process.env.ComSpec);
 
-// /** 用node执行某个文件 */
-// spawn('node', [path.join(__dirname, './c.js')], {
-//     stdio: 'inherit',
-// });
-
-let _ = execFile('node', [path.join(__dirname, './c.js')]);
-_.stdout.on('data', (d) => {
-    console.log(d.toString());
+/** 用node执行某个文件 */
+spawn('node', [path.join(__dirname, './c.js')], {
+    stdio: 'inherit',
 });
+//创建IPC服务
+let server = net
+    .createServer((socket) => {
+        console.log('连接');
+        let data = '';
+        socket.setEncoding('utf-8');
+        socket.on('data', (d) => {
+            console.log('收到数据', d);
+            data += d;
+        });
+        socket.on('end', () => {
+            console.log('数据传输完成', data);
+            // server.close();
+        });
+    })
+    .listen(path.join('\\\\?\\pipe', process.cwd(), 'myctl'));
+
+// let _ = execFile('node', [path.join(__dirname, './c.js')]);
+// _.stdout.on('data', (d) => {
+//     console.log(d.toString());
+// });
 
 // /** 用powershell查看目录结构 */
 // spawn('powershell', ['dir'], {
--- web+node/node/child_process/c.js
+++ web+node/node/child_process/c.js
@@ -1,2 +1,18 @@
 #!/usr/bin/env node
 console.log('我是文件c.js');
+const net = require('net');
+const path = require('path');
+
+// process.send?.(
+//     JSON.stringify({
+//         msg: 'c.js向父组件发送消息',
+//     }),
+// );
+
+let socket = net.connect(path.join('\\\\?\\pipe', process.cwd(), 'myctl'));
+
+socket.end(
+    JSON.stringify({
+        msg: 'c.js向父组件发送消息',
+    }),
+);
